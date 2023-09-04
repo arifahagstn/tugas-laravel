bce40315afac8b51d7729e39d624ca9ef94a1022
arifahagstn <arifahsri040805@gmail.com>
2023-09-04 17:55:52 +0700

    tugas laravel

--- resources/views/template/perpus/anggota.blade.php
+++ resources/views/template/perpus/anggota.blade.php
@@ -4,7 +4,7 @@
 
 <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Form Input Data Anggota</h3>
+                <h3 class="card-title">Form Output Data Anggota</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
